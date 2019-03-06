49a402e97c4eca95d7e3c3602089aaa9e96d30a0
xwp <xwp_fullstack@163.com>
2019-03-06 11:59:14 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect,render
 
+def index(request):
+    return HttpResponse('index')
 
-    def index(request):
-        return HttpResponse('index')
-
+def login(request):
+    return redirect('/index')
